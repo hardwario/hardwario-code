288b644880e2d4c69d2ae64cff0ad554d15f6baa
SmejkalJakub <smejkal.jakub99@gmail.com>
2022-08-04 18:22:39 +0200

    linux fix for data added

--- afterInstallScript.sh
+++ afterInstallScript.sh
@@ -4,3 +4,5 @@ chmod 777 -R /opt/hardwario-code/data
 mv /opt/hardwario-code/hardwario-code-icon.png /usr/share/icons/
 mv /opt/hardwario-code/hardwario-code.desktop /usr/share/applications/
 cp /opt/hardwario-code/data/tower/toolchain/SEGGER/JLink/99-jlink.rules /etc/udev/rules.d/99-jlink.rule
+mkdir ~/data
+cp -r /opt/hardwario-code/data/tower ~/data
