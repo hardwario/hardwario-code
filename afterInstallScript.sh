34ed94b3f75f69f6dd0e60ca1302b3b39429835b
SmejkalJakub <smejkal.jakub99@gmail.com>
2022-08-04 19:00:04 +0200

    jlink fixed for ubuntu

--- afterInstallScript.sh
+++ afterInstallScript.sh
@@ -3,6 +3,6 @@ chmod +rwx /opt/hardwario-code/code
 chmod 777 -R /opt/hardwario-code/data
 mv /opt/hardwario-code/hardwario-code-icon.png /usr/share/icons/
 mv /opt/hardwario-code/hardwario-code.desktop /usr/share/applications/
-cp /opt/hardwario-code/data/tower/toolchain/SEGGER/JLink/99-jlink.rules /etc/udev/rules.d/99-jlink.rule
+cp /opt/hardwario-code/data/tower/toolchain/SEGGER/JLink/99-jlink.rules /etc/udev/rules.d/
 mkdir ~/data
 cp -r /opt/hardwario-code/data/tower ~/data
