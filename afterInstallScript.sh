c46918c027e3ca1ee2a7bfcbd1308d0dfa461d6a
SmejkalJakub <smejkal.jakub99@gmail.com>
2022-08-04 17:49:41 +0200

    copy to desktop removed in linux version

--- afterInstallScript.sh
+++ afterInstallScript.sh
@@ -2,6 +2,5 @@ chmod o+rwx /opt/hardwario-code
 chmod +rwx /opt/hardwario-code/code
 chmod 777 -R /opt/hardwario-code/data
 mv /opt/hardwario-code/hardwario-code-icon.png /usr/share/icons/
-cp /opt/hardwario-code/hardwario-code.desktop ~/Desktop/
 mv /opt/hardwario-code/hardwario-code.desktop /usr/share/applications/
 cp /opt/hardwario-code/data/tower/toolchain/SEGGER/JLink/99-jlink.rules /etc/udev/rules.d/99-jlink.rule
