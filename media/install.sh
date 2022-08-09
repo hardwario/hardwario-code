e40dc4cfded7b2c4ae379edf8c87249d2c36cab8
SmejkalJakub <smejkal.jakub99@gmail.com>
2022-08-09 21:05:18 +0200

    sudo fixed for linux packages

--- media/install.sh
+++ media/install.sh
@@ -2,9 +2,7 @@
 
 folder="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-cp $folder/data/tower/toolchain/SEGGER/JLink/99-jlink.rules /etc/udev/rules.d/
-
-sudo -k
+sudo cp $folder/data/tower/toolchain/SEGGER/JLink/99-jlink.rules /etc/udev/rules.d/
 
 sed -i -e "s|code-folder|$folder|g" $folder/hardwario-code.desktop
 cp $folder/hardwario-code.desktop ~/.local/share/applications/
