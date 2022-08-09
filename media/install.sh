70da03a8e3868b79572d12b1b51612bbc2c1fe27
SmejkalJakub <smejkal.jakub99@gmail.com>
2022-08-09 20:38:06 +0200

    install script added to linux package

---
+++ media/install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+folder="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+sed -i -e "s|code-folder|$folder|g" $folder/hardwario-code.desktop
+cp $folder/hardwario-code.desktop ~/.local/share/applications/
+cp $folder/data/tower/toolchain/SEGGER/JLink/99-jlink.rules /etc/udev/rules.d/
